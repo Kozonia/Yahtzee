fecb17928704ee6f24ad035de384e1bf0b9efec2
Gavin Smith <131gms@gmail.com>
2025-07-15 19:56:42 -0500

    added implimentation of die class and set up game logic class

--- Die.cpp
+++ Die.cpp
@@ -1,2 +1,18 @@
 #include "Die.h"
+#include <iostream>
 using namespace std;
+
+Die::Die() : numOfSides(6)
+{
+	faceValue = 1;
+}
+
+void Die::roll()
+{
+	faceValue = rand() % 6 + 1;
+}
+
+const int Die::getFaceValue()
+{
+	return faceValue;
+}
--- GameLogic.h
+++ GameLogic.h
@@ -1,7 +1,26 @@
 #ifndef GAMELOGIC
 #define GAMELOGIC
+#include "Die.h"
+#include <iostream>
+
 class GameLogic
 {
+private:
+	Die* diceHand;
+	const int numOfDice;
+	int totalScore;
+	const int rounds;
+	int subRounds;
+	void printRolledValues();
+	void rollDiceHand();
+public:
+	GameLogic();
+	~GameLogic();
+	void playGame();
+	void printScore();
+	int getRounds();
+	int getSubRounds();
+	int getScore();
 
 };
 #endif
